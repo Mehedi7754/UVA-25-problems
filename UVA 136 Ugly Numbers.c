11ebc1588770de8351d5d6f34ce2f137527b4bf4
Mehedi7754 <155046042+Mehedi7754@users.noreply.github.com>
2024-01-21 22:09:24 +0600

    Update UVA 136 Ugly Numbers.c

--- UVA 136 Ugly Numbers.c
+++ UVA 136 Ugly Numbers.c
@@ -3,22 +3,28 @@ int main()
 {
     printf("The 1500'th ugly number is 859963392.\n");
    return 0;
-   /*calculation
+  /*
+//Calculation of finding the 1500'th ugly number.
 #include<stdio.h>
 int main()
 {
-    long long int num,i,count=0;
-
-    for(i=1;;i++){
+    long int num,i,count=0;
+    for(i=1;;i++)
+    {
         num=i;
-        while(num%2==0) num=num/2;
-        while(num%3==0) num=num/3;
-        while(num%5==0) num=num/5;
-        if(num==1)count++;
-        if(count==1500)break;
+        while(num!=1)
+        {
+            if(num%2==0) num=num/2;
+            else if(num%3==0) num=num/3;
+            else if(num%5==0) num=num/5;
+            else break;
+        }
+        if(num==1) count++;
+        if(count==1500) break;
     }
-   printf("%lld",i);
-   return 0;
-}*/
+    printf("The 1500'th ugly number is %ld.\n", i);
+    return 0;
+}
+*/
 
 }
